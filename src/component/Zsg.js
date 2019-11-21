eaad4946e0594c83cc33535b0b53db7d91faf9ca
lr0929 <liurong9@126.com>
2019-11-21 15:41:46 +0800

    测试git

---
+++ src/component/Zsg.js
@@ -0,0 +1,42 @@
+import React, {Component, Fragment} from 'react'
+
+class Zsg extends Component {
+    constructor(props){
+        super(props)
+        this.state= {
+            list:[],
+            inputV:''
+        }
+    }
+
+    aa(e){
+        this.setState({
+            inputV : e.target.value
+        })
+    }
+
+    tianjia(){
+        const list = [...this.state.list,this.state.inputV]
+        this.setState({
+            list
+        })
+    }
+
+    render() {
+        return(
+            <Fragment>
+                <input type="text"
+                       value={this.state.inputV}
+                       onChange={this.aa.bind(this)}
+                />
+                <button
+                onClick={this.tianjia.bind(this)}>
+                    提交
+                </button>
+
+            </Fragment>
+            )
+    }
+
+
+}
